4b402a4a5d12df7d1ef2592b21efcdc71f1850b2
Dmytro Bignyak <dmytro.bignyak@onapp.com>
2016-11-11 17:40:07 +0200

    Fix vcloud import

--- app/models/users_role.rb
+++ app/models/users_role.rb
@@ -2,7 +2,6 @@ class UsersRole < OnApp::Models::Base
   belongs_to :user
   belongs_to :role, counter_cache: :users_count
 
-  validates :user_id, presence: true
   validates :role_id, presence: true
-  validates :user_id, presence: true, uniqueness: { scope: :role_id }
+  validates :user_id, uniqueness: { scope: :role_id }
 end
