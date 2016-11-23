49606ca3892893ee8ccb3a6d372bef14ed75d665
Dmytro Bignyak <dmytro.bignyak@onapp.com>
2016-11-23 13:32:55 +0200

    CORE-8246 Fix restriction sets

--- app/models/restrictions/sets_role.rb
+++ app/models/restrictions/sets_role.rb
@@ -3,7 +3,6 @@ module Restrictions
     belongs_to :role
     belongs_to :set, class_name: 'Restrictions::Set'
 
-    validates :role_id, presence: true, uniqueness: { scope: :set_id }
-    validates :set_id, presence: true
+    validates :role_id, uniqueness: { scope: :set_id }
   end
 end
