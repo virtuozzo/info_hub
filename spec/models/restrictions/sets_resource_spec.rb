b2884e02f2239aef58d10ff66388dca49d0c43d7
Dmytro Bignyak <dmytro.bignyak@onapp.com>
2016-11-24 12:35:00 +0200

    Fix components tests

--- spec/models/restrictions/sets_resource_spec.rb
+++ spec/models/restrictions/sets_resource_spec.rb
@@ -5,30 +5,7 @@ describe Restrictions::SetsResource do
   let(:resource) { insert :restrictions_resource }
 
   describe '#validations' do
-    describe 'set_id' do
-      it 'is empty' do
-        sets_resource = Restrictions::SetsResource.new(resource: resource)
-
-        expect(sets_resource.save).to be_falsey
-        expect(sets_resource.errors).to include :set_id
-        expect(sets_resource.errors[:set_id]).to include "can't be blank"
-      end
-
-      it 'is valid' do
-        sets_resource = Restrictions::SetsResource.new(resource: resource, set: set)
-
-        expect(sets_resource.save).to be true
-      end
-    end
-
     describe 'resource_id' do
-      it 'is empty' do
-        sets_resource = Restrictions::SetsResource.new(set: set)
-
-        expect(sets_resource.save).to be_falsey
-        expect(sets_resource.errors).to include :resource_id
-        expect(sets_resource.errors[:resource_id]).to include "can't be blank"
-      end
 
       it 'is not uniq' do
         insert :restrictions_sets_resource, set: set, resource: resource
