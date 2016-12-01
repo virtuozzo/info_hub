4e7a6c935c92331dcf4e9756473ac47a1fd47010
Volodymyr Byno <volodymyr.byno@gmail.com>
2016-12-01 13:52:47 +0200

    CORE-8229 Update onapp-rom version

--- components/authorization/spec/authorization_logger_spec.rb
+++ components/authorization/spec/authorization_logger_spec.rb
@@ -4,6 +4,7 @@ describe SpecController, type: :controller do
   before { Rails.configuration.authorization_debug = true }
 
   describe '#default_authorized?' do
+    let(:logger)  { Rails.logger }
     let(:user)    { create :user }
     let(:action)  { :create }
     let(:aliases) { { index: :read, show: :read, new: :create,
@@ -15,15 +16,15 @@ describe SpecController, type: :controller do
     context 'instace' do
       context 'has permission' do
         it 'writes logs and performs action' do
-          expect(Rails.logger).to receive(:debug).with("Authorization. resource: #{user.inspect}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. action: #{action}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. mapping: #{aliases.with_indifferent_access.inspect}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. @resource_action: #{action}")
+          expect(logger).to receive(:debug).with("Authorization. resource: #{user.inspect}")
+          expect(logger).to receive(:debug).with("Authorization. action: #{action}")
+          expect(logger).to receive(:debug).with("Authorization. mapping: #{aliases.with_indifferent_access.inspect}")
+          expect(logger).to receive(:debug).with("Authorization. @resource_action: #{action}")
 
           get :authorized
 
           expect(response).to have_http_status :success
-          expect(response.body).to render_template text: 'Success!'
+          expect(response.body).to eq 'Success!'
         end
       end
 
@@ -31,15 +32,15 @@ describe SpecController, type: :controller do
         let(:action) { :edit }
 
         it 'writes specific logs and does not perform action' do
-          allow(Rails.logger).to receive(:warn).with("Was used unknown action `edit` for `users`")
+          allow(logger).to receive(:warn).with("Was used unknown action `edit` for `users`")
 
-          expect(Rails.logger).to receive(:debug).with("Authorization. resource: #{user.inspect}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. action: #{action}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. mapping: #{aliases.with_indifferent_access.inspect}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. @resource_action: #{action}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. User permissions: #{user.permission_identifiers.inspect}")
-          expect(Rails.logger).to receive(:debug).with('Authorization. User not authorized!')
-          expect(Rails.logger).to receive(:warn).with("Authorization. Required permission is \e[31musers.edit\e[0m")
+          expect(logger).to receive(:debug).with("Authorization. resource: #{user.inspect}")
+          expect(logger).to receive(:debug).with("Authorization. action: #{action}")
+          expect(logger).to receive(:debug).with("Authorization. mapping: #{aliases.with_indifferent_access.inspect}")
+          expect(logger).to receive(:debug).with("Authorization. @resource_action: #{action}")
+          expect(logger).to receive(:debug).with("Authorization. User permissions: #{user.permission_identifiers.inspect}")
+          expect(logger).to receive(:debug).with('Authorization. User not authorized!')
+          expect(logger).to receive(:warn).with("Authorization. Required permission is \e[31musers.edit\e[0m")
 
           get :unauthorized
 
@@ -51,15 +52,15 @@ describe SpecController, type: :controller do
     context 'class' do
       context 'has permission' do
         it 'writes logs and performs action' do
-          expect(Rails.logger).to receive(:debug).with("Authorization. resource: #{User.inspect}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. action: #{action}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. mapping: #{aliases.with_indifferent_access.inspect}")
-          expect(Rails.logger).to receive(:debug).with("Authorization. @resource_action: #{action}")
+          expect(logger).to receive(:debug).with("Authorization. resource: #{User.inspect}")
+          expect(logger).to receive(:debug).with("Authorization. action: #{action}")
+          expect(logger).to receive(:debug).with("Authorization. mapping: #{aliases.with_indifferent_access.inspect}")
+          expect(logger).to receive(:debug).with("Authorization. @resource_action: #{action}")
 
           get :class_authorized
 
           expect(response).to have_http_status :success
-          expect(response.body).to render_template text: 'Success!'
+          expect(response.body).to eq 'Success!'
         end
       end
     end
@@ -68,15 +69,15 @@ describe SpecController, type: :controller do
       let(:action) { :edit }
 
       it 'writes specific logs and does not perform action' do
-        allow(Rails.logger).to receive(:warn).with("Was used unknown action `edit` for `users`")
+        allow(logger).to receive(:warn).with("Was used unknown action `edit` for `users`")
 
-        expect(Rails.logger).to receive(:debug).with("Authorization. resource: #{User.inspect}")
-        expect(Rails.logger).to receive(:debug).with("Authorization. action: #{action}")
-        expect(Rails.logger).to receive(:debug).with("Authorization. mapping: #{aliases.with_indifferent_access.inspect}")
-        expect(Rails.logger).to receive(:debug).with("Authorization. @resource_action: #{action}")
-        expect(Rails.logger).to receive(:debug).with("Authorization. User permissions: #{user.permission_identifiers.inspect}")
-        expect(Rails.logger).to receive(:debug).with('Authorization. User not authorized!')
-        expect(Rails.logger).to receive(:warn).with("Authorization. Required permission is \e[31musers.edit\e[0m")
+        expect(logger).to receive(:debug).with("Authorization. resource: #{User.inspect}")
+        expect(logger).to receive(:debug).with("Authorization. action: #{action}")
+        expect(logger).to receive(:debug).with("Authorization. mapping: #{aliases.with_indifferent_access.inspect}")
+        expect(logger).to receive(:debug).with("Authorization. @resource_action: #{action}")
+        expect(logger).to receive(:debug).with("Authorization. User permissions: #{user.permission_identifiers.inspect}")
+        expect(logger).to receive(:debug).with('Authorization. User not authorized!')
+        expect(logger).to receive(:warn).with("Authorization. Required permission is \e[31musers.edit\e[0m")
 
         get :class_unauthorized
 
