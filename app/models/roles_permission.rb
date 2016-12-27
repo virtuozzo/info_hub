ed9a9895eafc763ca5205b66dea645f8d7a46f80
Max Lomanov <maxim.lomanov@onapp.com>
2016-12-27 15:38:06 +0200

    CORE-8562 Fix roles cloning

--- app/models/roles_permission.rb
+++ app/models/roles_permission.rb
@@ -1,7 +1,4 @@
 class RolesPermission < OnApp::Models::Base
   belongs_to :role
   belongs_to :permission
-
-  validates :role, presence: true
-  validates :permission, presence: true
 end
--- spec/models/roles_permission_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe RolesPermission do
-  it { is_expected.to validate_presence_of(:role_id) }
-  it { is_expected.to validate_presence_of(:permission_id) }
-end
