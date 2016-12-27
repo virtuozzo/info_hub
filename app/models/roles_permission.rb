c1f39c3831b9a810caf1c3d265eed4de593ac974
Max Lomanov <maxim.lomanov@onapp.com>
2016-12-27 12:52:55 +0200

    ORE-8562 Fix roles cloning

--- app/models/roles_permission.rb
+++ app/models/roles_permission.rb
@@ -2,6 +2,6 @@ class RolesPermission < OnApp::Models::Base
   belongs_to :role
   belongs_to :permission
 
-  validates :role_id, presence: true
-  validates :permission_id, presence: true
+  validates :role, presence: true
+  validates :permission, presence: true
 end
