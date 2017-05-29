333fd33d13eb286cc1d3dc3456b880390fd14562
Volodymyr Byno <volodymyr.byno@gmail.com>
2017-05-29 14:44:40 +0300

    CORE-9718 Remove networking:add_namespace raketask
    Rebase to development;

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,31 +1,17 @@
 unless ENV['SKIP_DATABASE_CLEANER']
   require 'database_cleaner'
 
-  def disable_referential_integrity
-    Sequel::DATABASES.first.run('SET FOREIGN_KEY_CHECKS=0;')
-    yield
-    Sequel::DATABASES.first.run('SET FOREIGN_KEY_CHECKS=1;')
-  end
-
   RSpec.configure do |config|
     config.use_transactional_fixtures = false
 
-    config.before(:suite) do
-      DatabaseCleaner[:active_record].strategy = :transaction
-      DatabaseCleaner[:sequel].strategy = :transaction if defined? Sequel
-
-      disable_referential_integrity do
-        DatabaseCleaner.clean_with :truncation
-      end
-
-      config.after(:each) { |example| sequel_cleaner.clean unless example.metadata[:skip_cleaner] }
-      config.before(:suite) { sequel_cleaner.clean_with :truncation }
+    config.before(:each) do |example|
+      # There are AR and ROM transactions therefore we can't use :transaction strategy
+      # despite the fact it is faster
+      DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :deletion
+      DatabaseCleaner.start unless example.metadata[:skip_cleaner]
     end
 
-    config.before(:all, truncate_before_all: true) do
-      disable_referential_integrity do
-        DatabaseCleaner.clean_with :truncation
-      end
-    end
+    config.after(:each) { |example| DatabaseCleaner.clean unless example.metadata[:skip_cleaner] }
+    config.before(:suite) { DatabaseCleaner.clean_with :truncation }
   end
 end
