61c67d4d86a2fa2b7d529f381c26245a5edf9771
Evgeniy Hinyuk <evgeniy.ginyuk@onapp.com>
2017-08-28 16:02:45 +0300

    CORE-10289 separate views for buckets resources

--- components/presenter/lib/presenter/view_helper.rb
+++ components/presenter/lib/presenter/view_helper.rb
@@ -3,7 +3,9 @@ require_relative 'class_detector'
 module Presenter
   module ViewHelper
     def present(object, presenter_class = nil, form = nil)
-      presenter = ClassDetector.presenter_class(object, presenter_class).new(object, self, form)
+      presenter = ClassDetector.
+                    presenter_class(object, presenter_class).
+                    new(object, self, form)
 
       yield presenter if block_given?
 
