8e15adce73d0a1de8717cac25aa41b38e53402ed
Yura Kuzhiy <yura.kuzhiy@onapp.com>
2017-08-28 16:46:13 +0300

    CORE-9632 SAML ldap attributes

--- app/controllers/core/users/sessions_controller.rb
+++ app/controllers/core/users/sessions_controller.rb
@@ -13,7 +13,23 @@ class Core::Users::SessionsController < Devise::SessionsController
     redirect_to core_engine.new_user_session_url, alert: t('devise.sessions.regular_login_disabled')
   end
 
+  def destroy
+    if logout_from_saml?
+      settings = current_user.saml_id_provider.settings
+      logout_request = OneLogin::RubySaml::Logoutrequest.new
+      reset_session
+
+      redirect_to logout_request.create(settings, RelayState: core_engine.new_user_session_url)
+    else
+      super
+    end
+  end
+
   private
+
+  def logout_from_saml?
+    session['saml_uid'] && current_user&.saml_id_provider&.idp_slo_target_url
+  end
 
   def force_saml_login?
     OnApp.configuration.force_saml_login_only
